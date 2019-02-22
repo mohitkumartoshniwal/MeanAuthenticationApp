5ac31c9fd5f6c6b8062a0bebeb5a042d1b71f827
mohitkumartoshniwal <mohitkrtoshniwal.com>
2019-02-22 21:14:01 +0530

    made user login , logout and protected routes for user left for vendor

--- angular-src/src/app/components/userlogin/userlogin.component.ts
+++ angular-src/src/app/components/userlogin/userlogin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-userlogin',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserloginComponent implements OnInit {
 
-  constructor() { }
+  username: String;
+  password: String;
+
+  constructor(private authService:AuthService,
+              private router:Router,) { }
 
   ngOnInit() {
+  }
+
+
+  onLoginSubmit(){
+    const user = {
+      username: this.username,
+      password: this.password
+    }
+
+    this.authService.authenticateUser(user).subscribe(data => {
+      if(data.success){
+        this.authService.storeUserData(data.token, data.user);
+        alert('You are now logged in');
+        this.router.navigate(['']);
+      } else {
+        alert(data.msg);
+        this.router.navigate(['userlogin']);
+      }
+    });
   }
 
 }
