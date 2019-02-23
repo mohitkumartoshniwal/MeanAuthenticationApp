592b2458839334cbb5ef149501abf631473d1b3e
3Dimensional Printing <onestop3dprintings@gmail.com>
2019-02-23 06:05:23 +0000

    Connected to mongoDB atlas (mlabs)

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://127.0.0.1:27017/3d',
+    database: 'mongodb://onestop:onestop@cluster0-shard-00-00-mtjqx.mongodb.net:27017,cluster0-shard-00-01-mtjqx.mongodb.net:27017,cluster0-shard-00-02-mtjqx.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
     secret: 'yoursecret'
 }
